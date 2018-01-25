5daff666a1080dad63a8a1b6758767833c292bd7
Jan Losinski <losinski@wh2.tu-dresden.de>
2018-01-25 12:15:38 +0100

    Test and tune dot effect.
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- src/EDot.cpp
+++ src/EDot.cpp
@@ -1,31 +1,52 @@
 #include "EDot.hpp"
 
 void EDot::update(LEDBand &band, unsigned long time) {
-    uint8_t hue = uint8_t(255.0f * float((time - start_time) % color_duration) / color_duration);
-
     int max = band.getLEDCount() / 2;
 
+    unsigned int dot_offset = max / count;
+    uint8_t hue = start_hue;
+    float hue_coeff = 0.0f;
+
+    if (0.0f < color_duration) {
+        hue = uint8_t(255.0f * float((time - start_time)%color_duration)/color_duration);
+        hue_coeff = float(duration)/float(color_duration)*(255.0f/float(max));
+    }
+
     unsigned int elapsed = (time - start_time) % duration;
-    float center = (float(elapsed) / float(duration)) * max;
-    int head = int8_t(ceilf(center));
-    int tail = int8_t(floorf(center)) - width;
+    float center = (float(elapsed * max) / float(duration)) + float(max);
+    int head = int(ceilf(center));
+    int tail = int(floorf(center)) - width;
 
     band.all_leds() = CRGB::Black;
 
+    unsigned int pos;
     unsigned int off = 0;
     do {
-        unsigned int pos = (tail + max + off++) % max;
-        float coeff = 1.0f - (fabsf(center - pos) / width);
-        CHSV color = CHSV(hue, 255, uint8_t(255 * coeff));
-        band.upper_leds()[pos] = color;
-        band.lower_leds()[pos] = color;
-    } while (tail + off != head);
+        pos = (tail + off++);
+        float center_distance = fabsf(center - pos);
+
+        float coeff = (pos == head
+            ? 1.0f - center_distance
+            : 1.0f - fminf(1.0f, center_distance / float(width)));
+        unsigned int actual_hue = (pos == head ? hue : hue - uint8_t(hue_coeff * center_distance));
+        uint8_t brightness = uint8_t(255 * coeff * coeff);
+        for (int i = 0; i < count; ++i) {
+           unsigned int actual_off = dot_offset * i;
+           CHSV color = CHSV(uint8_t((actual_hue + uint8_t(float(actual_off) * hue_coeff)) % 255),
+                            255, brightness);
+           unsigned int idx = (pos + actual_off) % max;
+           band.upper_leds()[idx] = color;
+           band.lower_leds()[idx] = color;
+        }
+    } while (pos != head);
 }
 
 
 void EDot::config(const ConfigWrapper &cfg) {
-    width = cfg.getOption(F("len"), 10);
+    width = cfg.getOption(F("len"), 10U);
     duration = cfg.getOption(F("speed"), 10U);
     color_duration = cfg.getOption(F("color_speed"), 15U);
+    count = cfg.getOption(F("count"), 1U);
+    start_hue = cfg.getOption(F("hue"), uint8_t(0));
     restart();
 }
--- src/EDot.hpp
+++ src/EDot.hpp
@@ -3,7 +3,7 @@
 
 class EDot : public Effect {
 public:
-  EDot() : width(0), start_time(0), duration(0) {}
+  EDot() : width(0), count(1), start_hue(0), start_time(0), duration(0) {}
 
   void update(LEDBand &band, unsigned long time) override;
   void restart() override {
@@ -12,7 +12,9 @@ public:
   void config(const ConfigWrapper &cfg) override;
 
 private:
-  int width;
+  unsigned int width;
+  unsigned int count;
+  uint8_t start_hue;
   unsigned long start_time;
   unsigned int duration;
   unsigned int color_duration;
