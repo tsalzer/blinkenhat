6aa462120f1be8394c235e9a81a6ac98bb462a5f
Jan Losinski <losinski@wh2.tu-dresden.de>
2018-02-03 11:41:40 +0100

    Fix web build script
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- scripts/web_ui.py
+++ scripts/web_ui.py
@@ -16,19 +16,21 @@ const unsigned long index_html_len = %(len)d;
 const char PROGMEM index_html[] = {%(index)s};
 """
 
+def build()
+    if not os.path.exists(INPUT):
+        print("No input file!")
+        return
 
-if not os.path.exists(INPUT):
-    print("No input file!")
-    sys.exit(0)
 
+    compessor = zlib.compressobj(9)
 
-compessor = zlib.compressobj(9)
+    with open(INPUT, "r") as fn:
+        contents = fn.read()
+        compressed = compessor.compress(contents) + compessor.flush()
+        formatted = ", ".join([hex(ord(ch)) for ch in compressed])
 
-with open(INPUT, "r") as fn:
-    contents = fn.read()
-    compressed = compessor.compress(contents) + compessor.flush()
-    formatted = ", ".join([hex(ord(ch)) for ch in compressed])
+        with open(OUTPUT, "w") as fo:
+            fo.write(TEMPLATE % {"index": formatted,
+                                 "len": len(compressed)})
 
-    with open(OUTPUT, "w") as fo:
-        fo.write(TEMPLATE % {"index": formatted,
-                             "len": len(compressed)})
+build()
